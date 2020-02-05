549d8c816dd9ceff991dcf0df5b58e1248ab2dbb
amyhsieh16 <46662655+amyhsieh16@users.noreply.github.com>
2020-02-05 19:06:02 +0800

    Add note & adjust code
    增加備註
    修改變數名稱
    使用python3呼叫方式

--- stock-test.py
+++ stock-test.py
@@ -13,18 +13,26 @@ def getStackInfo(date):
     # 下載股價
     stockInfo_All = requests.get('https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + date + '&type=ALL')
     return stockInfo_All
+
 def sortOutData(data):
     #設定column顯示的數量
     pd.set_option("display.max_columns", None)
+    #pd.set_option('max_colwidth', 800)
     #讓資料對齊
     pd.set_option('display.unicode.ambiguous_as_wide', True)
     pd.set_option('display.unicode.east_asian_width', True)
 
-    # 整理資料，變成表格
-    df = pd.read_csv(StringIO(data.text.replace("=", "")), 
-            header=["證券代號" in l for l in data.text.split("\n")].index(True)-1)
+    #整理資料，變成表格
+    #read_csv("檔案位置或 file-like object(因此需要StringIO)"
+    #         ,找到allStockInfo的起始(由)證劵代號在data.text中
+    #         ,如果想要完整資料則需加入 delimiter="\n"                                                )
+            
+
+    df = pd.read_csv(StringIO(data.text.replace("=", "")),
+                     header=["證券代號" in L for L in data.text.split("\n")].index(True)-1)
+    
     return df.head()
 
 
-if __name__ == "__main__":
-    main()
+#呼叫main function
+main()
